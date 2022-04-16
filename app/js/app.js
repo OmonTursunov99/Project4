15f232c71f44d4d0a657a4a4a5713672fba00770
OmonTursunov99 <94193911+OmonTursunov99@users.noreply.github.com>
2022-04-16 12:54:30 +0300

    commit

--- app/js/app.js
+++ app/js/app.js
@@ -2,18 +2,58 @@ let mainHeaderMediaButton = document.querySelector('.main-header-media-button');
 let mainHeaderButton = document.querySelector('#mainHeaderButton');
 let mainHeaderMedia = document.querySelector('.main-header-media');
 let mainHeaderMediaLinkGroup = document.querySelectorAll('.main-header-media-link');
-let mainHeaderNavigationsLinkGroup = document.querySelectorAll('.main-header-navigations-link');
 let mainLoading = document.querySelector('.main-loading');
-let a = document.querySelector('.main-header-navigations').querySelector('ul').querySelectorAll('a');
-a.forEach(item => {
-    console.log(item)
+let mainHeaderNavLinksGroup = document.querySelector('.main-header-navigations').querySelectorAll('a');
+let mainHeaderNavMediaLinksGroup = document.querySelector('.main-header-media').querySelector('nav').querySelectorAll('a');
+let mainFooterNavLinksGroup = document.querySelector('.nav-unordered').querySelectorAll('a');
+
+mainHeaderMediaLinkGroup.forEach(link => {
+    let linkPathName = link.pathname;
+    let wPathName = window.location.pathname;
+    if(wPathName === linkPathName) {
+        link.classList.add('active')
+    }
+    link.onclick = function () {
+        removeClassNav();
+    }
+});
+mainHeaderNavLinksGroup.forEach(link => {
+    let linkPathName = link.pathname;
+    let wPathName = window.location.pathname;
+    if(wPathName === linkPathName) {
+        link.classList.add('active')
+    }
+    link.onclick = function () {
+        removeClassNav();
+    }
+})
+mainHeaderNavMediaLinksGroup.forEach(link => {
+    let linkPathName = link.pathname;
+    let wPathName = window.location.pathname;
+    if(wPathName === linkPathName) {
+        link.classList.add('active')
+    }
+    link.onclick = function () {
+        removeClassNav();
+    }
+})
+mainFooterNavLinksGroup.forEach(link => {
+    let linkPathName = link.pathname;
+    let wPathName = window.location.pathname;
+    if(wPathName === linkPathName) {
+        link.classList.add('active')
+    }
+    link.onclick = function () {
+        removeClassNav();
+    }
 })
 
-let loadElements = () => {
+document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         mainLoading.classList.add('not-active');
     }, 700)
-}
+})
+
 window.addEventListener('scroll', () => {
     let mainHeader = document.querySelector('.main-header');
     let mainUpButton = document.querySelector('.main-up-button');
@@ -37,20 +77,14 @@ let removeClassNav = () => {
 mainHeaderButton.addEventListener('click', () => {
     toggleClassNav();
 });
-mainHeaderMediaLinkGroup.forEach(link => {
-    let linkPathName = link.pathname;
-    let wPathName = window.location.pathname;
-    if(wPathName === linkPathName) {
-        link.classList.add('activePath')
-    }
-    link.onclick = function () {
+window.addEventListener('click', e => { // при клике в любом месте окна браузера
+    const target = e.target // находим элемент, на котором был клик
+    console.log(!target.closest('#mainHeaderButton'))
+    if (!target.closest('.main-header-media') && !target.closest('#mainHeaderButton')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
         removeClassNav();
     }
-});
-mainHeaderNavigationsLinkGroup.forEach(link => {
-    let linkPathName = link.pathname;
-    let wPathName = window.location.pathname;
-    if(wPathName === linkPathName) {
-        link.classList.add('activePath')
-    }
-});
+})
+
+
+
+
